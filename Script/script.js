c6eb813c737088d325a5ef8d6ad297d0d85ae189
Nutella9775 <adrienbarbet7@gmail.com>
2025-09-16 17:18:01 +0200

    fix: insertion script.js in body

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,10 @@
+let victoires = 0;
+let defaites = 0;
+let nuls = 0;
 
-alert("Bienvenue dans le jeu de Chifoumi !");
+const choix = ["Pierre", "Papier", "Ciseaux"];
+const resultat = document.getElementById("resultat");
+const victoiresEl = document.getElementById("victoires");
+const defaitesEl = document.getElementById("defaites");
+const nulsEl = document.getElementById("nuls");
+
